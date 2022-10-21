01293323ab597267acf5072eabaf30879f0c74c5
bensalah-sec <bensalah.nabil@etudiant-fst.utm.tn>
2022-10-21 17:16:56 +0100

    portfolio

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,25 +1,43 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio3.jpg'
+
+
+
+const data = [
+  {
+    id : 1,
+    image: IMG1,
+    title: 'Supermarket Management System',
+    github:'https://github.com/bensalah-nabil/Product-cash-Management',
+    demo:'https://dribble.com/shots/16673715-Crypto-currency-dashboards-and-Financial-data-visualization'
+  }
+]
 
 const portfolio = () => {
   return (
     <section id ='portfolio'>
       <h5>A selection of my best projects</h5>
       <h2>Portfolio</h2>
-    
-      <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <h3>Supermarket Management System</h3>
-            
-          </div>
-
+      {
+        data.map(({id, image, title, github, demo}) => {
+          return (
+            <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo } className='btn btn-primary' target='_blank'>Live Demo</a>
+              </div>
+            </div>
         </article>
+          )})
+      }
 
-      </div>
     </section>
-    
   )
+     
 } 
 
 export default portfolio
